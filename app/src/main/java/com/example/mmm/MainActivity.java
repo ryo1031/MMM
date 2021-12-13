548045c96746aceb71200add2afd910b847e9bc1
0120kan <eijirou0120@gmail.com>
2021-12-13 17:04:50 +0900

    mmm

--- app/src/main/java/com/example/mmm/MainActivity.java
+++ app/src/main/java/com/example/mmm/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    //mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+    //mmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
